3a2ff45c0154bc82bc53fa11e71fa91f93c1dd77
LRA <lra@r73.info>
2018-07-17 07:41:49 +0200

    vault PoC added

---
+++ KeyVaultImportPoC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KeyVaultImportPoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
